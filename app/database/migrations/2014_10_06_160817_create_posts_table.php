7f19899f95a0c20b0aba97b0078f8d0f20764334
Clement Rigo <clementr@innervisiongroup.com>
2014-10-06 18:24:24 +0200

    create table for posts

---
+++ app/database/migrations/2014_10_06_160817_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->string('title');
+			$table->text('content');
+			$table->integer('status'); // 0 = draft // 1 = published
+			$table->string('url');
+			$table->boolean('allow_comments');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
