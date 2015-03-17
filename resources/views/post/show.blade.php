8112da9ba427402684235d7333ed461bfeae06a3
Clément Rigo <clementr@innervisiongroup.com>
2015-03-17 23:43:45 +0100

    add highlight js support

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,6 +3,10 @@
 @section('meta-title', $post->title)
 @section('meta-subtitle', 'Posted by '.link_to_route('user.show', $post->user->username, $post->user->username).' on '.date('M d Y', strtotime($post->created_at)))
 
+@section('styles')
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/default.min.css">
+@stop
+
 @section('content')
 
     <article>
@@ -30,4 +34,9 @@
         <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
     @endif
 
+@stop
+
+@section('scripts')
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
 @stop
