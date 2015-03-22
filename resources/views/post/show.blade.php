daf9712e0cd388c4057c885a461a187392c0fa4d
Clément Rigo <clementr@innervisiongroup.com>
2015-03-22 01:38:31 +0100

    duh that was in double

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -11,12 +11,12 @@
 
     <article>
         <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+            <div class="col-md-12">
                 {!! $post->content !!}
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+            <div class="col-md-12">
                 @foreach ($post->tags as $tag)
                     {!! link_to_route('tag.show', '#'.$tag->name, $tag->slug) !!}
                 @endforeach
