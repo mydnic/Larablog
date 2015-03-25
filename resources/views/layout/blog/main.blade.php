ee8ddb5f78f3928326878f253c3358f0fad07b0b
Clément Rigo <clementr@innervisiongroup.com>
2015-03-25 01:34:38 +0100

    fix footer style

--- resources/views/layout/blog/main.blade.php
+++ resources/views/layout/blog/main.blade.php
@@ -56,7 +56,7 @@
         <hr>
 
         <!-- Footer -->
-        <footer>
+        <footer class="footer">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
