752d20599035280418c033f2ed9a87f6615b1be6
Clément Rigo <rigoclement@mydnic.be>
2015-05-07 11:55:24 +0200

    more efficient seo

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,7 +3,7 @@
 @section('meta-title', $post->title)
 @section('meta-subtitle', 'Posted by '.link_to_route('user.show', $post->user->username, $post->user->username).' on '.date('M d Y', strtotime($post->created_at)))
 @section('meta-image', url($post->picture))
-@section('meta-description', str_limit(strip_tags($post->content), 140))
+@section('meta-description', str_limit(strip_tags($post->title), 140))
 @section('meta-url', URL::route("post.show", $post->slug))
 
 @section('styles')
