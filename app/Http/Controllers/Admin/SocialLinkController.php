3760c2412849ff8aaf1a4e6185c3a1c1b43edf10
Clément Rigo <rigoclement@mydnic.be>
2015-09-21 12:03:35 +0200

    admin can edit link

--- app/Http/Controllers/Admin/SocialLinkController.php
+++ app/Http/Controllers/Admin/SocialLinkController.php
@@ -1,95 +1,104 @@
 <?php namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests;
 use App\SocialLink;
 use Illuminate\Support\Facades\Redirect;
 use Laracasts\Flash\Flash;
 use Request;
 
-class SocialLinkController extends Controller {
+class SocialLinkController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$links = SocialLink::all();
-		return view('admin.settings.social.index')
-			->with('links', $links);
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $links = SocialLink::all();
+        return view('admin.settings.social.index')
+            ->with('links', $links);
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('admin.settings.social.create');
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('admin.settings.social.create');
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		$link = new SocialLink;
-		$link->title = Request::get('title');
-		$link->url = Request::get('url');
-		$link->icon = Request::get('icon');
-		$link->save();
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $link        = new SocialLink;
+        $link->title = Request::get('title');
+        $link->url   = Request::get('url');
+        $link->icon  = Request::get('icon');
+        $link->save();
 
-		Flash::success('Link successfully added');
-		return Redirect::route('admin.settings.social.index');
-	}
+        Flash::success('Link successfully added');
+        return Redirect::route('admin.settings.social.index');
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $link = SocialLink::find($id);
+        return view('admin.settings.social.edit')
+            ->with('link', $link);
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        $link        = SocialLink::find($id);
+        $link->title = Request::get('title');
+        $link->url   = Request::get('url');
+        $link->icon  = Request::get('icon');
+        $link->save();
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+        Flash::success('Link successfully updated');
+        return Redirect::back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
