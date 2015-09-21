1cfdb8d2cb03da5a5db2e0957215096fa2f8aedb
Clément Rigo <rigoclement@mydnic.be>
2015-09-21 10:00:55 +0200

    Update slug for ProjectCategory Model

--- app/ProjectCategory.php
+++ app/ProjectCategory.php
@@ -1,11 +1,19 @@
-<?php namespace App;
+<?php
+
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use MartinBean\Database\Eloquent\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
 class ProjectCategory extends Model {
 
-    use Sluggable;
+    use SluggableTrait;
+
+    protected $sluggable = [
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    ];
 
     public function projects()
     {
