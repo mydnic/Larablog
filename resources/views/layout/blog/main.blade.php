750209185367b71f8962fb925b5d5632925e5d6a
Clément Rigo <rigoclement@mydnic.be>
2016-04-18 15:25:35 +0200

    Add Favicon

--- resources/views/layout/blog/main.blade.php
+++ resources/views/layout/blog/main.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" href="/uploads/{{ $setting->favicon }}" type="image/png"/>
 
         <title>@yield('meta-title', $setting->app_name)</title>
 
